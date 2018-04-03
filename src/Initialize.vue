724fc081ee9031eafe24081c6dc1e870293a6f82
sashakost <sashakost@me.com>
2018-04-03 19:52:57 +0300

    Modify Initialize.vue

---
+++ src/Initialize.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <button @click="init">Initialize</button>
+    <button @click="get">Get</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'InitializeData',
+    methods: {
+      init () {
+        this.$store.dispatch('users/seed')
+        this.$store.dispatch('conversations/seed')
+      },
+      get () {
+      }
+    }
+  }
+</script>
+
+<style>
+  button {
+    font-size: 1.25rem;
+    background: transparent;
+    border: 2px solid black;
+    padding: 12px 18px;
+  }
+</style>
---
+++ src/config.js
@@ -0,0 +1,9 @@
+const config = {
+  apiKey: 'AIzaSyCKKsxAu_MBd7tpDDTdxtbulnLuDrXWu-Y',
+  authDomain: 'vue-chat-64fe6.firebaseapp.com',
+  databaseURL: 'https://vue-chat-64fe6.firebaseio.com',
+  projectId: 'vue-chat-64fe6',
+  storageBucket: 'vue-chat-64fe6.appspot.com',
+  messagingSenderId: '618803225926'
+}
+export default config
